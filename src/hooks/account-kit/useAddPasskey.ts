70a32de07ba6e4d5723519164ea5261d53246727
alejandro waumann <awaumann@gmail.com>
2025-08-04 19:24:42 -0500

    feat(account-kit-hooks): add useAddPasskey and useRemovePasskey

---
+++ src/hooks/account-kit/useAddPasskey.ts
@@ -0,0 +1,34 @@
+import { useMutation, type UseMutateFunction } from "@tanstack/react-query";
+import { useSigner } from "./useSigner";
+import { useAccountKitContext } from "../useAccountKitContext";
+
+export type UseAddPasskeyResult = {
+  addPasskey: UseMutateFunction<string[], Error, void, unknown>;
+  isAddingPasskey: boolean;
+  addPasskeyError: Error | null;
+};
+
+export function useAddPasskey(): UseAddPasskeyResult {
+  const { queryClient } = useAccountKitContext();
+  const signer = useSigner();
+
+  const {
+    mutate: addPasskey,
+    isPending: isAddingPasskey,
+    error: addPasskeyError,
+  } = useMutation(
+    {
+      mutationFn: async () => {
+        if (!signer) {
+          throw new Error("useAddPasskey: No signer");
+        }
+
+        return signer.addPasskey();
+      },
+      mutationKey: ["addPasskey"],
+    },
+    queryClient,
+  );
+
+  return { addPasskey, isAddingPasskey, addPasskeyError };
+}
---
+++ src/hooks/account-kit/useRemovePasskey.ts
@@ -0,0 +1,34 @@
+import { useMutation, type UseMutateFunction } from "@tanstack/react-query";
+import { useAccountKitContext } from "../useAccountKitContext";
+import { useSigner } from "./useSigner";
+
+export type UseRemovePasskeyResult = {
+  removePasskey: UseMutateFunction<void, Error, string, unknown>;
+  isRemovingPasskey: boolean;
+  removePasskeyError: Error | null;
+};
+
+export function useRemovePasskey(): UseRemovePasskeyResult {
+  const { queryClient } = useAccountKitContext();
+  const signer = useSigner();
+
+  const {
+    mutate: removePasskey,
+    isPending: isRemovingPasskey,
+    error: removePasskeyError,
+  } = useMutation(
+    {
+      mutationFn: async (authenticatorId: string) => {
+        if (!signer) {
+          throw new Error("useRemovePasskey: No signer");
+        }
+
+        return signer.removePasskey(authenticatorId);
+      },
+      mutationKey: ["removePasskey"],
+    },
+    queryClient,
+  );
+
+  return { removePasskey, isRemovingPasskey, removePasskeyError };
+}
